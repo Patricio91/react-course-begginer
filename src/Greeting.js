93b23a9351c46ab51842b923bfcf1e4e274b84bf
Patricio91 <patricioponcini10@gmail.com>
2023-03-01 22:35:52 -0300

    First commit - Props in React

---
+++ src/Greeting.js
@@ -0,0 +1,16 @@
+export function Greeting({ title, name = "User" }) {
+    return <h2>{title}, dice {name}</h2>
+}
+
+export function UserCard({ name, amount, married, adress, greet }) {
+    console.log(name, amount, married, adress, greet)
+    return <div>
+        <h1>Name: {name}</h1>
+        <p>Amount: 💲{amount}</p>
+        <p>{married ? "I'm married. Don't talk to me" : "I'm single baby, only for you"}</p>
+        <ul>
+            <li>City: {adress.city}</li>
+            <li>Street: {adress.street}</li>
+        </ul>
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Greeting, UserCard } from "./Greeting";
+import Product, { NavBar } from "./Product";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// Siempre debe haber un elemento que contenga al resto, por eso los div's
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+        <UserCard
+            name="Patri Poncini"
+            amount={10000}
+            married={true}
+            points={[99, 33, 22, 10]}
+            adress={{ street: "123 Main Street", city: "San Francisco" }}
+            greet={function () { alert("Hello. Stop right there") }}
+        />
+        <UserCard
+            name="Sir Galahad"
+            amount={20000}
+            married={false}
+            points={[12, 14, 55, 13]}
+            adress={{ street: "234 Main Street", city: "Colorado" }}
+            greet={function () { alert("Hello. What are you doing?") }}
+        />
+    </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
