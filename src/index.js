b1958ff4dc930ffad8f4c6e82a2a8f18bf458896
Patricio91 <patricioponcini10@gmail.com>
2023-03-11 11:38:43 -0300

    React Hooks: useState

--- src/index.js
+++ src/index.js
@@ -1,37 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // Siempre debe haber un elemento que contenga al resto, por eso los div's para contener elementos
 
-const users = [
-    {
-        id: 1,
-        name: "Patricio",
-        image: "https://robohash.org/user1"
-    },
-    {
-        id: 5,
-        name: "Federico",
-        image: "https://robohash.org/user5"
-    },
-    {
-        id: 10,
-        name: "Federico",
-        image: "https://robohash.org/user10"
-    }
-]
+// const users = [
+//     {
+//         id: 1,
+//         name: "Patricio",
+//         image: "https://robohash.org/user1"
+//     },
+//     {
+//         id: 5,
+//         name: "Federico",
+//         image: "https://robohash.org/user5"
+//     },
+//     {
+//         id: 10,
+//         name: "Federico",
+//         image: "https://robohash.org/user10"
+//     }
+// ]
 
+function Counter() {
+    const [message, setMessage] = useState();
+
+    // return (
+    //     <div>
+    //         <h1>Counter: {counter}</h1>
+    //         <button onClick={() => { setCounter(counter + 10) }}>
+    //             Sumar
+    //         </button>
+    //         <button onClick={() => { setCounter(counter - 10) }}>
+    //             Restar
+    //         </button>
+    //         <button onClick={() => { setCounter(0) }}>
+    //             Reiniciar
+    //         </button>
+    //     </div>
+    // )
+
+    return (
+        <div>
+            <input onChange={(e => setMessage(e.target.value))} />
+            <button onClick={() => { alert("The message: " + message) }}>
+                Send
+            </button>
+        </div>
+    )
+}
 
 root.render(
     <>
-        {users.map((user, i) => {
+        <Counter />
+        {/* {users.map((user, i) => {
             return (
                 <div key={i}>
                     <h1>{user.name}</h1>
                     <img src={user.image} />
                 </div>
             );
-        })}
+        })} */}
         {/* <Post /> */}
 
         {/* <TaskCard ready={true} />
